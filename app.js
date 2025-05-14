bfeb0ab50858b050ad41512dca15ebe3d8b8f834
Hrach Kaderyan Midence <johndoe@example.com>
2025-05-14 12:41:37 -0400

    Formatear código

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import routes from './src/controllers/users.js';
+import routes from "./src/controllers/users.js";
 
-import { client } from './src/database/config.js';
-import createUserTable from './src/database/migrations.js';
+import { client } from "./src/database/config.js";
+import createUserTable from "./src/database/migrations.js";
 
 const app = express();
 
@@ -15,18 +15,20 @@ app.use(cors());
 
 app.use(routes);
 
-app.get('/', (req, res) => {
-    res.send('Server online');
+app.get("/", (req, res) => {
+  res.send("Server online");
 });
 
-client.connect()
-    .then(() => {
-        console.info('Base de datos conectada');
-        createUserTable(client)
-            .then(() => {
-                app.listen(3000, () => {
-                    console.info('Server online in port 3000');
-                });
-            });
-    })
-    .catch(err => console.error(`Error al conectar con la base de datos: ${err}`))
+client
+  .connect()
+  .then(() => {
+    console.info("Base de datos conectada");
+    createUserTable(client).then(() => {
+      app.listen(3000, () => {
+        console.info("Server online in port 3000");
+      });
+    });
+  })
+  .catch((err) =>
+    console.error(`Error al conectar con la base de datos: ${err}`)
+  );
